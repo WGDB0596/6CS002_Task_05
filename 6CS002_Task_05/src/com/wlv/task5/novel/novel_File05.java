fa893f35e1650da29ee04109e601de0e6bc9ef59
win10 <win10@DESKTOP-V4LH47O>
2022-07-16 11:32:09 +0530

    Novel Case - Herewith used .sorted() function to sort the lines that include in the story. Here with sorted according to the length of lines.

---
+++ 6CS002_Task_05/src/com/wlv/task5/novel/novel_File05.java
@@ -0,0 +1,22 @@
+package com.wlv.task5.novel;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class novel_File05 {
+
+	public static void main(String[] args) throws Exception 
+	{
+	BufferedReader r = new BufferedReader(new FileReader("data/Hell fer Sartain.txt"));
+	r.lines().sorted((a,b)-> {
+		if(a.length() == b.length())
+			return 0;
+		if(a.length()<b.length())
+			return 1;
+		return -1;
+		})
+	.forEach(l-> System.out.println(l));
+	r.close(); 
+	
+	}
+}
